956821121f4ffe690a58cd3d0a3c466324e00ae5
OlegPomazkov <povcode@gmail.com>
2016-02-20 13:03:08 +0300

    Before sending to GitHub

--- js/slider.js
+++ js/slider.js
@@ -15,7 +15,29 @@ function PageController() {
 				   sliders[1].initValue,	
 				   sliders[2].initValue,
 				   sliders[3].initValue,
-		];
+		],
+		elemsWithTooltips = document.querySelectorAll("[tooltip]"),
+		tooltipWindows = document.querySelectorAll(".tooltip"),
+		tooltipButtons = document.querySelectorAll(".tooltipButton");
+				
+/* Инициализация слушателей, чтобы показать подсказку на элементах управления                  */
+	for(var i = 0; i < elemsWithTooltips.length; i++) {
+		elemsWithTooltips[i].addEventListener("mouseenter", showTooltip);
+		elemsWithTooltips[i].addEventListener("mouseleave", hideTooltip);		
+	};
+
+/* Инициализация слушателей, на кнопках управления окном интерактивной подсказки               */
+	for(var i = 0; i < tooltipButtons.length; i++) {
+		tooltipButtons[i].addEventListener("click", function(){
+					for(var i = 0; i < tooltipButtons.length; i++) {
+						tooltipButtons[i].classList.toggle("hidden");						
+					};	
+					for(i = 0; i < tooltipWindows.length; i++) {
+						tooltipWindows[i].classList.toggle("hidden");						
+					};
+		});		
+	};
+	
 /* Инициализация основного экрана начальными значениями                                        */
 	setCurrentScreenColor(colors);
 	
@@ -27,8 +49,14 @@ function PageController() {
 	document.querySelector(".standartColors").addEventListener("click", setLastColor)
 	
 /* Установка фонового изображения и инициализация "слушателя" на панели фоновых изображений    */
-	setScreenBackground("url(images/black.png)");
+	setScreenBackground("url(images/baw.png)");
 	document.querySelector(".backgroundsPanel").addEventListener("click", setBackground);
+
+/* Установка слушателя на кнопку запоминания текущего цвета                                    */
+	document.querySelector(".button").addEventListener("click", function(){
+			setLastColorScreen( "rgb(" + colors.slice(0,3).join(",") + ")");
+	});
+
 
 /* Построение слайдеров и инициализация слушателей на них                                      */
 	renderSlidersPanel(sliders);
@@ -43,6 +71,7 @@ function PageController() {
 			setCurrentScreenColor(colors);
 		};
 	};
+	
 };
 
 /* --------------------------------------------------------------------------------------------*/
@@ -56,7 +85,24 @@ function PageController() {
 	function setBackground(e) {		
        if(!(e.target.tagName === "IMG")) return;
 	   setScreenBackground(e.target.getAttribute("data-image"));		
-	};			
+	};
+/* --------- Демонстратор соответствующей подсказки в окне интерактивной подсказки ----------- */	
+	function showTooltip(e){
+		var tooltipText = [
+			"Основная панель управления параметрами цвета. Для выбора нужного значения 'перетаскивайте' мышью бегунок соответсвующего слайдера. Отображаемые в окошках значения соответствуют формату rgba(R,G,B,O)",
+			"Панель стандартных цветов позволяет быстро установить выбранный цвет с указанием его параметров в формате rgb(R,G,B) на дополнительный экран. Для этого достаточно 'кликнуть' мышью на выбранном цвете",
+			"Набор стандартных фоновых изображений для оценки отображения основного цвета при значениях прозрачности менее 1. Для установки любого изображения достаточно 'кликнуть' мышью на выбранном элементе",
+			"Основной экран - 2-слойный: нижний слой представляет собой один из 4 предлагаемых фоновых риcунков, верхний - монохромный с параметрами цвета, которые устанавливаются при помощи слайдеров слева. Запомнить текущее состояние, записав цвет на дополнительный экран, можно нажатием кнопки в верхней части.",
+			"Дополнительный экран позволяет сравнить выбранный цвет с цветом, отображаемым на большом экране. Выбор цвета дополнительного экрана осуществляется нажатием соответствующего цвета на панели справа. Сохранить текущий цвет большого экрана на дополнительном можно нажатием кнопки сверху."
+		];
+		var tooltipScreen = document.querySelector(".tooltip");	
+		tooltipScreen.innerHTML = tooltipText [+e.target.getAttribute("tooltip")];
+	};
+/* --------- "Чистильщик" окна интерактивной подсказки --------------------------------------- */	
+	function hideTooltip(e){
+		var tooltipScreen = document.querySelector(".tooltip");
+		tooltipScreen.innerHTML = "</br></br></br>Наведите курсор на элементы управления для получения подсказки";
+	};
 	
 /* --------------------------------------------------------------------------------------------*/
 /* ------------- ФУНКЦИИ СОЗДАНИЯ И ИНИЦИАЛИЗАЦИИ ЭЛЕМЕНТОВ ИНТЕРФЕЙСА ----------------------- */
@@ -133,7 +179,7 @@ function PageController() {
 		
 		for( var i = 0; i < sliders.length; i++) {
 			elem = document.createElement("div");
-			elem.classList.add(".slider");
+			elem.classList.add("slider");
 			elem.setAttribute("data-name", sliders[i].name);
 			elem.innerHTML = tmpl({name:sliders[i].name});
 			elem = container.appendChild(elem);
@@ -162,9 +208,7 @@ function PageController() {
 		
 			if (!e.target.classList.contains("marker")) return;
 			
-			document.onmousedown = function() {
-				return false;
-			};
+			e.preventDefault();
 			document.addEventListener("mousemove", mousemoveHandler);			
 			document.addEventListener("mouseup", mouseupHandler);
 
@@ -176,7 +220,6 @@ function PageController() {
 		};
 
 		function mouseupHandler() {
-			document.onmousedown = null;
 			document.removeEventListener("mousemove", mousemoveHandler);
 			document.removeEventListener("mouseup", mouseupHandler);
 		};
